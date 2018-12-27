a61770b24712a0514f08982447b94698ab134efe
LoomisKoo <286305974@qq.com>
2018-12-27 11:53:09 +0800

    Initial commit

---
+++ swipeRecyclerview/src/main/java/android.support.v7.widget.helper/MyCompatItemTouchHelper.java
@@ -0,0 +1,20 @@
+package android.support.v7.widget.helper;
+
+/**
+ * Created by Yan Zhenjie on 2016/8/1.
+ */
+public class MyCompatItemTouchHelper extends ItemTouchHelper {
+
+    public MyCompatItemTouchHelper(ItemTouchHelper.Callback callback) {
+        super(callback);
+    }
+
+    /**
+     * Developer callback which controls the behavior of ItemTouchHelper.
+     *
+     * @return {@link Callback}
+     */
+    public ItemTouchHelper.Callback getCallback() {
+        return mCallback;
+    }
+}
